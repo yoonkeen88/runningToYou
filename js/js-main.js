5613dce9419901f1cda717fe93465f4e64576ca0
yoonkeen <agy91521947@gmail.com>
2024-11-05 13:36:51 +0900

    다시 수정

--- js/js-main.js
+++
@@ -1,8 +0,0 @@
-// Main JavaScript file that initializes everything
-import { initFullpage } from './fullpage-config.js';
-import { initEmailHandler } from './email-handler.js';
-
-document.addEventListener('DOMContentLoaded', function() {
-    initFullpage();
-    initEmailHandler();
-});
--- js/js-utils.js
+++
@@ -1,9 +0,0 @@
-// Utility functions
-export function validateEmail(email) {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-}
-
-export function encryptEmail(email) {
-    return btoa(email); // Base64 encoding
-}
